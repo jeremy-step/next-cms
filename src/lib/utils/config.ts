e487dac1da0d718a30fd05febc2b994ac00d905f
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-10-01 14:05:34 +0200

    Created website config

---
+++ src/lib/utils/config.ts
@@ -0,0 +1,37 @@
+import websiteConfig from "@/website.config";
+
+export function getConfig<T, B = null>(
+  path?: string,
+  defaultValue: unknown = null
+): T | B {
+  if (path === undefined) {
+    return websiteConfig as T;
+  }
+
+  const paths = path.split(".");
+  const valueFirst = (websiteConfig as never)[paths[0]];
+
+  if (
+    valueFirst === undefined ||
+    (paths.length > 1 && typeof valueFirst !== "object")
+  ) {
+    return defaultValue as B;
+  }
+
+  let config = valueFirst;
+
+  for (let index = 1; index < paths.length; index++) {
+    const value = config[paths[index]] ?? undefined;
+
+    if (
+      value === undefined ||
+      (paths.length - 1 !== index && typeof value !== "object")
+    ) {
+      return defaultValue as B;
+    }
+
+    config = value;
+  }
+
+  return config as T;
+}
---
+++ website.config.ts
@@ -0,0 +1,19 @@
+const websiteConfig = {
+  controlPanel: {
+    title: "Control Panel",
+    meta: {
+      description: "Sistema de gestión de contenido",
+    },
+  },
+
+  front: {
+    title: "Universae FCT Olyverse NextCMS",
+    meta: {
+      description: "Sistema de gestión de contenido",
+    },
+  },
+
+  app: {},
+};
+
+export default websiteConfig;
