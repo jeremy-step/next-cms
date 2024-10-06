8f6b5175772769edd4076340efbf432df1782606
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-10-06 22:34:32 +0200

    Moved editor to Editor Component

--- src/ui/ControlPanel/components/app/pages/PageForm.tsx
+++ src/ui/ControlPanel/components/app/pages/PageForm.tsx
@@ -2,28 +2,17 @@
 
 import { createPage, PageFormState } from "@lib/actions/ControlPanel/pages";
 import InputField from "../../forms/InputField";
-import { MDXEditor } from "../../MDXEditor";
 import { useFormState } from "react-dom";
-import { useRef, useState } from "react";
-import { MDXEditorMethods } from "@mdxeditor/editor";
+import MDXEditorField from "../../forms/MDXEditorField";
 
 export default function PageForm({ ...props }) {
   const initialState: PageFormState = { message: null, errors: {} };
   const [state, formAction] = useFormState(createPage, initialState);
 
-  const contentRef = useRef<MDXEditorMethods>(null);
-
-  const [content, setContent] = useState("");
-
-  const handleSetContent = async () => {
-    setContent(contentRef.current?.getMarkdown() ?? "");
-  };
-
   return (
     <form {...props} className="grid gap-5 mt-5" action={formAction}>
       <InputField name="title" label="Title" state={state} />
-      <MDXEditor markdown="" ref={contentRef} onChange={handleSetContent} />
-      <input type="hidden" name="content" defaultValue={content} />
+      <MDXEditorField name="content" label="Content" state={state} />
       <button>Submit</button>
     </form>
   );
