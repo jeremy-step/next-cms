3c88c48c0a106e25a3d016546306d8218e9a42a5
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-10-06 22:34:41 +0200

    UI

--- src/app/(back)/control-panel/pages/[pageId]/page.tsx
+++ src/app/(back)/control-panel/pages/[pageId]/page.tsx
@@ -1,9 +1,15 @@
+import { getLink } from "@lib/utils/router";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Edit Page",
 };
 
 export default function Page() {
-  return <div>Edit Page</div>;
+  return (
+    <div>
+      Edit Page <Link href={getLink("cp.pages/create")}>Create Page</Link>
+    </div>
+  );
 }
